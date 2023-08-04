f246ab8bfe7056a057404b7aafc8c65869c09b23
Alex Felipe <alexfelipevieira@gmail.com>
2023-08-04 08:59:49 -0300

    finishing the first layout

--- app/src/main/java/br/com/alura/techtaste/screens/HomeScreen.kt
+++ app/src/main/java/br/com/alura/techtaste/screens/HomeScreen.kt
@@ -1,6 +1,5 @@
 package br.com.alura.techtaste.screens
 
-import android.widget.Space
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -26,9 +25,11 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.ColorPainter
@@ -56,32 +57,14 @@ val categories = mapOf(
     Icons.Filled.CheckCircle to "Bebidas",
 )
 
-val meals = listOf(
+val meals = List(10) {
     Meal(
         name = LoremIpsum(Random.nextInt(2, 10)).values.first(),
         description = LoremIpsum(Random.nextInt(10, 20)).values.first(),
         price = BigDecimal(Random.nextDouble(300.0)),
         calories = Random.nextDouble(3000.0)
-    ),
-    Meal(
-        name = LoremIpsum(Random.nextInt(2, 10)).values.first(),
-        description = LoremIpsum(Random.nextInt(10, 20)).values.first(),
-        price = BigDecimal(Random.nextDouble(300.0)),
-        calories = Random.nextDouble(3000.0)
-    ),
-    Meal(
-        name = LoremIpsum(Random.nextInt(2, 10)).values.first(),
-        description = LoremIpsum(Random.nextInt(10, 20)).values.first(),
-        price = BigDecimal(Random.nextDouble(300.0)),
-        calories = Random.nextDouble(3000.0)
-    ),
-    Meal(
-        name = LoremIpsum(Random.nextInt(2, 10)).values.first(),
-        description = LoremIpsum(Random.nextInt(10, 20)).values.first(),
-        price = BigDecimal(Random.nextDouble(300.0)),
-        calories = Random.nextDouble(3000.0)
-    ),
-)
+    )
+}
 
 private const val TAG = "HomeScreen"
 
@@ -93,65 +76,146 @@ fun HomeScreen() {
             .verticalScroll(rememberScrollState())
     ) {
         CategoriesSection()
-        MostPopularSection()
+        MealSection(title = {
+            Text(
+                text = "Mais populares",
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }, Modifier.padding(24.dp)) {
+            for (meal in meals.shuffled().subList(1, 3)) {
+                Column(
+                    Modifier
+                        .clip(RoundedCornerShape(12.dp))
+                        .background(Cinza1),
+                ) {
+                    AsyncImage(
+                        model = "https://picsum.photos/${Random.nextInt(1280, 1920)}/${
+                            Random.nextInt(
+                                720,
+                                1920
+                            )
+                        }",
+                        contentDescription = "imagem da refeição",
+                        Modifier
+                            .fillMaxWidth()
+                            .height(160.dp)
+                            .clip(
+                                RoundedCornerShape(
+                                    topStart = 12.dp,
+                                    topEnd = 12.dp
+                                )
+                            ),
+                        contentScale = ContentScale.Crop,
+                        placeholder = ColorPainter(Color.Gray),
+                    )
+                    Column(Modifier.padding(16.dp)) {
+                        Text(
+                            text = meal.name,
+                            color = LaranjaMedio,
+                            fontSize = 16.sp
+                        )
+                        Text(
+                            text = meal.price.round(MathContext(2)).toPlainString(),
+                            color = Cinza3,
+                            fontSize = 14.sp
+                        )
+                        Spacer(modifier = Modifier.height(16.dp))
+                        Text(
+                            text = meal.description,
+                            color = Cinza3,
+                            fontSize = 14.sp
+                        )
+                    }
+                }
+            }
+            TextButton(onClick = { /*TODO*/ }) {
+                Text(text = "Ver mais")
+            }
+        }
+        Spacer(
+            modifier = Modifier
+                .padding(horizontal = 24.dp)
+                .height(1.dp)
+                .fillMaxWidth()
+                .background(Color(0xFF49454F))
+                .alpha(0.5f)
+        )
+        MealSection(title = {
+            Text(
+                text = "Delícias vegeratianas",
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }, Modifier.padding(24.dp)) {
+            for (meal in meals.shuffled().subList(1, 3)) {
+                Column(
+                    Modifier
+                        .clip(RoundedCornerShape(12.dp))
+                        .background(Cinza1),
+                ) {
+                    AsyncImage(
+                        model = "https://picsum.photos/${Random.nextInt(1280, 1920)}/${
+                            Random.nextInt(
+                                720,
+                                1920
+                            )
+                        }",
+                        contentDescription = "imagem da refeição",
+                        Modifier
+                            .fillMaxWidth()
+                            .height(160.dp)
+                            .clip(
+                                RoundedCornerShape(
+                                    topStart = 12.dp,
+                                    topEnd = 12.dp
+                                )
+                            ),
+                        contentScale = ContentScale.Crop,
+                        placeholder = ColorPainter(Color.Gray),
+                    )
+                    Column(Modifier.padding(16.dp)) {
+                        Text(
+                            text = meal.name,
+                            color = LaranjaMedio,
+                            fontSize = 16.sp
+                        )
+                        Text(
+                            text = meal.price.round(MathContext(2)).toPlainString(),
+                            color = Cinza3,
+                            fontSize = 14.sp
+                        )
+                        Spacer(modifier = Modifier.height(16.dp))
+                        Text(
+                            text = meal.description,
+                            color = Cinza3,
+                            fontSize = 14.sp
+                        )
+                    }
+                }
+            }
+            TextButton(onClick = { /*TODO*/ }) {
+                Text(text = "Ver mais")
+            }
+        }
     }
 }
 
 @Composable
-fun MostPopularSection() {
-    Text(
-        text = "Mais populares",
-        Modifier.padding(horizontal = 24.dp),
-        fontSize = 22.sp,
-        fontWeight = FontWeight.Bold
-    )
-    Column(modifier = Modifier.padding(24.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp)) {
-        for (meal in meals) {
-            Column(
-                Modifier
-                    .clip(RoundedCornerShape(12.dp))
-                    .background(Cinza1),
-            ) {
-                AsyncImage(
-                    model = "https://picsum.photos/${Random.nextInt(1280, 1920)}/${
-                        Random.nextInt(
-                            720,
-                            1920
-                        )
-                    }",
-                    contentDescription = "imagem da refeição",
-                    Modifier
-                        .fillMaxWidth()
-                        .height(160.dp)
-                        .clip(
-                            RoundedCornerShape(
-                                topStart = 12.dp,
-                                topEnd = 12.dp
-                            )
-                        ),
-                    contentScale = ContentScale.Crop,
-                    placeholder = ColorPainter(Color.Gray),
-                )
-                Column(Modifier.padding(16.dp)) {
-                    Text(
-                        text = meal.name,
-                        color = LaranjaMedio,
-                        fontSize = 16.sp
-                    )
-                    Text(
-                        text = meal.price.round(MathContext(2)).toPlainString(),
-                        color = Cinza3,
-                        fontSize = 14.sp
-                    )
-                    Spacer(modifier = Modifier.height(16.dp))
-                    Text(
-                        text = meal.description,
-                        color = Cinza3,
-                        fontSize = 14.sp
-                    )
-                }
-            }
+fun MealSection(
+    title: @Composable () -> Unit,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Column(
+        modifier,
+        verticalArrangement = Arrangement.spacedBy(24.dp)
+    ) {
+        title()
+        Column(
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            content()
         }
     }
 }
